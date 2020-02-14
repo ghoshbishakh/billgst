f23c3e34f8d7a7e6a33a3011ddcb73db6abf0baf
Abhijit Mondal <abhijit.manpur@gmail.com>
2020-02-14 19:54:58 +0530

    windows executable

---
+++ python-3.8.1-embed-amd64/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\SRMLab\Documents\bishakh\gstbilling\python-3.8.1-embed-amd64\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
