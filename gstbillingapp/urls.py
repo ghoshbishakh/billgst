e074b8ce5e8a22531457f7610e6941d3b6f70941
Bishakh Ghosh <ghoshbishakh@gmail.com>
2020-06-10 18:51:59 +0530

    Delete invoice with inventory update

--- gstbillingapp/urls.py
+++ gstbillingapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),
 
     path('invoices/new', views.invoice_create, name='invoice_create'),
+    path('invoices/delete', views.invoice_delete, name='invoice_delete'),
 
     path('login', views.login_view, name='login_view'),
     path('signup', views.signup_view, name='signup_view'),
